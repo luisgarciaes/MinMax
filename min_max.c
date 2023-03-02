17965998354ca298325377934b3ef5be1ebc8b88
ruruisu <zaddozgl@gmail.com>
2023-03-02 16:51:14 -0600

    fixed variable scope

--- min_max.c
+++ min_max.c
@@ -1,21 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
-int* create_array() {
-    int sz = 0;
-    printf("Enter the size of array::");
-    scanf("%d", &sz);
+int* create_array(int sz, int* pSize) {
     int* randArr = malloc(sz * sizeof(int));
+    srand(time(NULL)); // seed the random number generator
     int i;
     for (i = 0; i < sz; i++)
-        randArr[i] = rand() % 1;
+        randArr[i] = rand() % 100; // generate random numbers between 0 and 99
+    *pSize = sz; // store the size of the array in the memory location pointed to by pSize
     return randArr;
 }
 
 int main() {
-    int* arr = create_array();
-    int i, sz;
-    sz = sizeof(arr) / sizeof(int);
+    int sz = 0;
+    printf("Enter the size of array: ");
+    scanf("%d", &sz);
+    int* arr = create_array(sz, &sz); // pass the size of the array and a pointer to sz
+    int i;
     for (i = 0; i < sz; i++)
         printf("%d ", arr[i]);
     free(arr);
