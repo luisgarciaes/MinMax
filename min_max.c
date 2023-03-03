5e2312b9143a7a81f694b5435a149d970020a724
ruruisu <zaddozgl@gmail.com>
2023-03-02 18:18:31 -0600

    aesthetic fixes

--- min_max.c
+++ min_max.c
@@ -4,6 +4,7 @@
 
 #include <time.h>
 
+//function for the bubble sorting suggested in the instructions
 void bubble_sort(int arr[], int size) {
     int i, j, temp;
     for (i = 0; i < size - 1; i++) {
@@ -16,7 +17,7 @@ void bubble_sort(int arr[], int size) {
         }
     }
 }
-
+// this function creates a random array with custom size
 int* create_array(int sz, int* pSize) {
     int* randArr = malloc(sz * sizeof(int));
     srand(time(NULL));
@@ -26,31 +27,41 @@ int* create_array(int sz, int* pSize) {
     *pSize = sz;
     return randArr;
 }
-int* find_min_max(int arr[]) {
-    int max_index = sizeof(arr) / sizeof(arr[0]) - 1;
+
+// this function takes an array and returns the maximum value and the minimum value
+int* find_min_max(int arr[], int length) {
+    int max_index = length - 1;
     int min = arr[0];
-    int max = arr[0];
-    printf("Minimum value in array: ");
-    printf("%d", min);
+    int max = arr[max_index];
+    printf("\nMinimum value in array: ");
+    printf("%d\n", min);
     printf("Maximum value in array: ");
-    printf("%d ", max);
+    printf("%d\n ", max);
 }
 int main() {
+    printf("  /\\_/\\\n");
+    printf(" ( o.o ) MINMAX ARRAY PROGRAM\n");
+    printf("  > ^ <  MADE BY LUIS GERARDO GARCIA ESPINOZA\n");
+    printf("\n////////////////////////////////////////////////////\n\n");
     int sz = 0;
     char input[10];
 
-    printf("Enter the size of array: ");
+    printf("Enter the size of array:\n");
     fgets(input, sizeof(input), stdin);
     if (sscanf_s(input, "%d", &sz) != 1) {
         printf("Invalid input. Please enter an integer value.\n");
         return 1;
     }
+    printf("\n////////////////////////////////////////////////////\n\n");
     int* arr = create_array(sz, &sz);
     int i;
     bubble_sort(arr, sz);
+    printf("Here is the sorted array: \n");
+    printf("[ ");
     for (i = 0; i < sz; i++)
-        printf("%d ", arr[i]);
-    find_min_max(arr);
+        printf("%d, ", arr[i]);
+    printf("]");
+    find_min_max(arr,sz);
     free(arr);
     return 0;
 }
